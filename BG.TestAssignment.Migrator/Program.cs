ae980824abc588893dae884420de3bbc891d4e21
zona003 <zona0031256@gmail.com>
2023-10-29 20:28:59 +0200

    Create migrator

---
+++ BG.TestAssignment.Migrator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BG.TestAssignment.Migrator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
