3d5c5580d6c0c80c3945abc0ed49f8e3662f77f1
zona003 <zona0031256@gmail.com>
2023-11-01 19:08:11 +0200

    Add old angular project to project

--- BG.TestAssignment.Ui/src/app/app.component.ts
+++ BG.TestAssignment.Ui/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'BG.TestAssignment.Ui';
+
+constructor(private as:AuthService) {
+}
+
+  public get isLoggedIn(): boolean{
+    return this.as.isAutenticated();
+  }
+
+  login(userName: string, password: string){
+    this.as.login(userName, password)
+    .subscribe({ error: (e) => alert('Wrong login or password.')
+    });
+  }
+
+  logout(){
+    this.as.logout();
+  }
+  
 }
---
+++ BG.TestAssignment.Ui/src/app/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production : false,
+    authApi: 'http://localhost:5278',
+    booksApi: 'http://localhost:5062',
+    tokenWhitelistedDomains: ['localhost:5278']
+};
