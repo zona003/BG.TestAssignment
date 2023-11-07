20adcca2d5a9c2a5b0dd6d89a693cf7e94d7a5a9
zona003 <zona0031256@gmail.com>
2023-11-07 19:41:56 +0200

    Chenge frontend structure

--- BG.TestAssignment.Ui/src/app/common/_helpers/jwtinterceptor.ts
+++ BG.TestAssignment.Ui/src/app/common/_helpers/jwtinterceptor.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { environment } from '../environments/environment';
-import { AuthService } from '../services/auth.service';
-import { ResponceWrapper } from '../models/responceWrapper';
+import { environment } from '../../environments/environment';
+import { AuthService } from '../../services/auth.service';
+import { ResponceWrapper } from '../../models/responceWrapper';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
